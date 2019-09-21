d1659a9f8cc9e067cb48b23a49231a67a816d96b
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-09-21 10:34:39 +0530

    custom prompt trigger and event register

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,15 +1,36 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  createPostArea.style.display = "block";
+  // Ask for prompt if browser support install banner
+  if (deferredPrompt) {
+    // Ask for prompt
+    deferredPrompt.prompt();
+
+    // check user's selection
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
+
+      // closed or deny prompt
+      if (choiceResult.outcome === "dismissed") {
+        console.log("user cancelled installation");
+      } else {
+        console.log("user added to home screen");
+      }
+    });
+
+    deferredPrompt = null;
+  }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
