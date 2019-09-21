6f214e7e0b267a0224856accc2e6bd0b56742004
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-09-21 10:27:27 +0530

    Add fetch event listener for service worker

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,9 @@ self.addEventListener("activate", function(event) {
   console.log("[Service Worker]: Activating service worker", event);
   return self.clients.claim();
 });
+
+self.addEventListener("fetch", function(event) {
+  console.log("[Service Worker]: Fetching something...", event);
+  // Override response
+  event.respondWith(fetch(event.request));
+});
