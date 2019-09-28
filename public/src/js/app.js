76dff586ec366f114347048626173a50daa1fe0e
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-09-28 19:36:39 +0530

    Add promise and fetch polyfills

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -6,10 +6,20 @@
 
 var deferredPrompt;
 
+// Enabling promise in older browsers.
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(function() {
-    console.log("Service worker registered");
-  });
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function() {
+      console.log("Service worker registered");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 window.addEventListener("beforeinstallprompt", function(event) {
